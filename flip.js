ea8c1b3e0cc35bacd96461393e3f9e5ed38e783a
Maxwell Mullin <mullinmax@gmail.com>
2019-06-21 23:17:22 -0400

    Add files via upload

---
+++ flip.js
@@ -0,0 +1,78 @@
+// set game size
+var size = 3
+
+// create rules
+var rules = []
+for(var c = 0; c < size; c++){
+    rules[c] = []
+    for(var r = 0; r < size; r++){
+        rules[c][r] = createRule(c, r, size);
+    }
+}
+console.log(rules)
+// draw board
+cells = []
+for(var c = 0; c < size; c++){
+    cells[c] = []
+    for(var r = 0; r < size; r++){
+        cells[c][r] = true;
+        cell = `<div id='r${r}c${c}' class='cell' onClick='applyRule(${r},${c})'></div>`
+        document.getElementById('flip').innerHTML += cell;
+    }
+}
+for(var i = 0; i < 3; i++){
+    var row = Math.floor(Math.random()*size);
+    var col = Math.floor(Math.random()*size);
+    applyRule(row, col);
+    updateBoard();
+}
+
+// run game
+function applyRule(row, col){
+    console.log(`applying rule ${col}, ${row}`);
+    for(var c = 0; c < size; c++){
+        for(var r = 0; r < size; r++){
+            fun = rules[col][row][c][r]
+            if (fun == 'W'){
+                cells[c][r] = true;
+            } else if(fun == 'B'){
+                cells[c][r] = false;
+            } else if(fun == 'F'){
+                cells[c][r] = !cells[c][r];
+            } else if(fun == 'N'){
+                cells[c][r] = cells[c][r];
+            }
+        }
+    }
+    updateBoard()
+}
+
+function updateBoard(){
+    for(var c = 0; c < size; c++){
+        for(var r = 0; r < size; r++){
+            if (cells[c][r]){
+                document.getElementById(`r${r}c${c}`).classList = 'cell';
+            }else{
+                document.getElementById(`r${r}c${c}`).classList = 'cell flip';
+            }
+        }
+    }
+}
+
+// helper functions
+function createRule(col, row, s){
+    var rule = [];
+    // funs = ['F', 'B', 'W', 'N'];
+    funs = ['F', 'N', 'N'];
+    for(var c = 0; c < s; c++){
+        rule[c] = []
+        for(var r = 0; r < s; r++){
+            var index = Math.floor(Math.random()*funs.length);
+            // console.log(index, funs[index])
+            rule[c][r] = funs[index];
+        }
+
+    }
+    rule[col][row] = funs[0]
+    return rule
+}
